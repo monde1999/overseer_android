b5ecc2c70bf1339930b020b0fbaa003bfcdce729
Franz <franzlaconsay11@gmail.com>
2021-09-04 00:11:00 +0800

    Add zoom to device location.

--- app/src/main/java/com/adventurers/overseer/map/views/MapActivity.java
+++ app/src/main/java/com/adventurers/overseer/map/views/MapActivity.java
@@ -2,6 +2,7 @@ package com.adventurers.overseer.map.views;
 
 import static com.adventurers.overseer.Constants.RC_GPS_SERVICE;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,9 +14,15 @@ import com.adventurers.overseer.R;
 import com.adventurers.overseer.map.helpers.Location;
 import com.adventurers.overseer.map.helpers.PermissionHelper;
 import com.adventurers.overseer.map.helpers.StatusBarHelper;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.List;
 
@@ -56,9 +63,49 @@ public class MapActivity extends FragmentActivity
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
         PermissionHelper.requestLocationAndGPS(this);
-        //mMap.setMyLocationEnabled(true);
+        startActivity();
     }
 
+    @SuppressLint("MissingPermission")
+    private void startActivity() {
+        if(PermissionHelper.isLocationGranted(this) && PermissionHelper.isGPSOn(this) && mMap != null) {
+            mMap.setMyLocationEnabled(true);
+            findUserLocation();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void findUserLocation() {
+        FusedLocationProviderClient fusedLocationClient =
+                LocationServices.getFusedLocationProviderClient(this);
+
+        // Gets last known location and zoom to it using successListener
+        // Fast but less accurate, used to zoom in faster
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, successListener);
+    }
+
+    private final OnSuccessListener<android.location.Location>
+            successListener = new OnSuccessListener<android.location.Location>() {
+        @Override
+        public void onSuccess(android.location.Location location) {
+            if(location != null) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                zoomToLatLng(latLng, 15, false);
+            }
+        }
+    };
+
+    private void zoomToLatLng(LatLng latLng, float zoomLevel, boolean animated) {
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel);
+        if(animated){
+            mMap.animateCamera(cameraUpdate);
+        }
+        else {
+            mMap.moveCamera(cameraUpdate);
+        }
+    }
+
+    //region : Permissions
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
@@ -72,6 +119,7 @@ public class MapActivity extends FragmentActivity
     public void onPermissionsGranted(int requestCode, @NonNull List<String> list) {
         // Some permissions have been granted
         PermissionHelper.requestLocationAndGPS(this);
+        startActivity();
     }
 
     @Override
@@ -95,12 +143,14 @@ public class MapActivity extends FragmentActivity
         if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
             // Do something after user returned from app settings screen.
             PermissionHelper.requestLocationAndGPS(this);
+            startActivity();
         }
         else if(requestCode == RC_GPS_SERVICE) {
             // Do something after GPS is turned on in location dialog
             switch (resultCode) {
                 case Activity.RESULT_OK:
                     PermissionHelper.requestLocationAndGPS(this);
+                    startActivity();
                     break;
                 case Activity.RESULT_CANCELED:
                     PermissionHelper.requestLocationAndGPS(this);
@@ -108,4 +158,5 @@ public class MapActivity extends FragmentActivity
             }
         }
     }
+    // endregion
 }
