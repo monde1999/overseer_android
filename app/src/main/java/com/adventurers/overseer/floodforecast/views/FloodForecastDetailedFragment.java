3a2114dc340e9f9f0fa9bb50772d1ccb93fd6ad1
Franz Laconsay <franzlaconsay11@gmail.com>
2021-09-24 15:45:29 +0800

    Mao and FloodForecast server integration update.

--- app/src/main/java/com/adventurers/overseer/floodforecast/views/FloodForecastDetailedFragment.java
+++ app/src/main/java/com/adventurers/overseer/floodforecast/views/FloodForecastDetailedFragment.java
@@ -12,6 +12,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
